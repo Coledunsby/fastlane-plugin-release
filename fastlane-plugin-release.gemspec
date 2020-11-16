bedf6ee0eb37269298be92ab3e29f9c2dc48ff96
Cole Dunsby <cole.dunsby@shoebox.md>
2020-11-16 13:17:42 -0500

    WIP

--- fastlane-plugin-release.gemspec
+++ fastlane-plugin-release.gemspec
@@ -18,6 +18,8 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ['lib']
 
+  spec.add_dependency 'fastlane-plugin-remove_git_tag'
+
   spec.add_development_dependency('pry')
   spec.add_development_dependency('bundler')
   spec.add_development_dependency('rspec')
--- lib/fastlane/plugin/release/actions/make_release_action.rb
+++ lib/fastlane/plugin/release/actions/make_release_action.rb
@@ -1,4 +1,5 @@
 require 'fastlane/action'
+require 'fastlane/plugin/remove_git_tag'
 require_relative '../helper/make_release_helper'
 
 module Fastlane
@@ -8,65 +9,85 @@ module Fastlane
         class MakeReleaseAction < Action
 
             def self.run(params)
-                other_action.ensure_git_branch(branch: params[:ensure_git_branch])
-                other_action.ensure_git_status_clean if params[:ensure_git_status_clean]
 
-                if params[:pre_bump]
-                    version = bump_version(params, params[:version])
-                end
+                other_action.ensure_git_branch
+                other_action.ensure_git_status_clean
 
-                version = tag_and_release(params)
+                Actions.sh("git remote update")
+                local_commit = Actions.sh("git rev-parse HEAD")
+                remote_commit = Actions.sh("git rev-parse @{u}")
+                UI.abort_with_message! "Your branch is behind 'origin/master' (use \"git pull\" to update your local branch)" unless local_commit == remote_commit
 
-                if params[:post_bump]
-                    version = bump_version(params, nil)
-                end
+                podspec_version = other_action.version_get_podspec(path: File.expand_path(params[:podspec]))
+                project_version = other_action.get_version_number(xcodeproj: File.expand_path(params[:xcodeproj]))
+                UI.abort_with_message! "Podspec version (#{podspec_version}) does not match project version (#{project_version})" unless podspec_version == project_version
 
-                version
-            end
-
-            def self.bump_version(params, version)
-                podspec = File.expand_path(params[:podspec])
-
-                if version.nil?
+                if params[:version]
                     version = other_action.version_bump_podspec(
-                        path: podspec,
-                        bump_type: params[:version_bump_type]
+                        path: File.expand_path(params[:podspec]),
+                        version_number: params[:version]
                     )
                 else
-                    other_action.version_bump_podspec(
-                        path: podspec,
-                        version_number: version
+                    version = other_action.version_bump_podspec(
+                        path: File.expand_path(params[:podspec]),
+                        bump_type: params[:version_bump_type]
                     )
                 end
 
-                xcodeproj = File.expand_path(params[:xcodeproj])
-
-                unless xcodeproj.nil?
-                    other_action.increment_version_number(version_number: version, xcodeproj: xcodeproj)
-                    other_action.increment_build_number(xcodeproj: xcodeproj) if params[:bump_build]
-                end
-
-                other_action.commit_version_bump(include: [params[:podspec]])
-                other_action.push_to_git_remote
-
-                version
-            end
-
-            def self.tag_and_release(params)
-                podspec = File.expand_path(params[:podspec])
-                version = other_action.version_get_podspec(path: podspec)
-
-                other_action.add_git_tag(tag: params[:tag_prefix] + version)
-                other_action.push_to_git_remote
-
-                other_action.pod_push(
-                    path: podspec,
-                    repo: params[:podspec_repo],
-                    allow_warnings: params[:allow_warnings],
-                    sources: params[:sources]
+                other_action.increment_version_number(
+                    version_number: version,
+                    xcodeproj: File.expand_path(params[:xcodeproj])
                 )
 
-                version
+                if params[:increment_build_number]
+                    if params[:build_number]
+                        other_action.increment_build_number(
+                            build_number: params[:build_number],
+                            xcodeproj: File.expand_path(params[:xcodeproj])
+                        )
+                    else
+                        other_action.increment_build_number(
+                            xcodeproj: File.expand_path(params[:xcodeproj])
+                        )
+                    end
+                end
+
+                other_action.commit_version_bump(
+                    message: params[:version_bump_commit_message],
+                    include: params[:podspec]
+                )
+
+                tag = "#{params[:tag_prefix]}#{version}"
+
+                begin
+                    other_action.add_git_tag(tag: tag)
+                rescue => ex
+                    Actions.sh("git reset --hard #{local_commit}")
+                    raise
+                end
+
+                begin
+                    other_action.push_to_git_remote
+                rescue => ex
+                    Actions.sh("git reset --hard #{local_commit}")
+                    other_action.remove_git_tag(tag: tag)
+                    raise
+                end
+
+                begin
+                    other_action.pod_push(
+                        path: File.expand_path(params[:podspec]),
+                        repo: params[:podspec_repo],
+                        allow_warnings: params[:podspec_allow_warnings],
+                        sources: params[:podspec_sources]
+                    )
+                rescue => ex
+                    Actions.sh("git reset --hard #{local_commit}")
+                    other_action.remove_git_tag(tag: tag)
+                    other_action.push_to_git_remote(force: true)
+                    raise
+                end
+
             end
 
             def self.description
@@ -88,75 +109,51 @@ module Fastlane
             def self.available_options
                 [
                     FastlaneCore::ConfigItem.new(
-                        key: :allow_warnings,
-                        env_name: "ALLOW_WARNINGS",
-                        default_value: true,
-                        description: "Allow warnings during pod push",
-                        optional: true,
-                        type: Boolean
-                    ),
-                    FastlaneCore::ConfigItem.new(
-                        key: :bump_build,
-                        env_name: "BUMP_BUILD",
-                        default_value: false,
-                        description: "Will increment build number if enabled",
-                        optional: true,
-                        type: Boolean
-                    ),
-                    FastlaneCore::ConfigItem.new(
-                        key: :ensure_git_branch,
-                        env_name: "ENSURE_GIT_BRANCH",
-                        default_value: "master",
-                        description: "The branch that should be checked for. String that can be either the full name of the branch or a regex to match",
+                        key: :build_number,
+                        env_name: "FL_RELEASE_BUILD_NUMBER",
+                        description: "Change to a specific build number",
                         optional: true,
                         type: String
                     ),
                     FastlaneCore::ConfigItem.new(
-                        key: :ensure_git_status_clean,
-                        env_name: "ENSURE_GIT_STATUS_CLEAN",
-                        default_value: true,
-                        description: "Raises an exception if there are uncommitted git changes",
+                        key: :increment_build_number,
+                        env_name: "FL_RELEASE_INCREMENT_BUILD_NUMBER",
+                        default_value: false,
+                        description: "Will increment build number if enabled",
                         optional: true,
-                        type: Boolean
+                        is_string: false
                     ),
                     FastlaneCore::ConfigItem.new(
                         key: :podspec,
-                        env_name: "PODSPEC",
+                        env_name: "FL_RELEASE_PODSPEC",
+                        default_value: Dir["*.podspec"].first,
+                        default_value_dynamic: true,
                         description: "The path of the podspec file to update",
-                        optional: false,
+                        optional: true,
                         type: String,
                         verify_block: proc do |value|
                             UI.user_error!("Could not find podspec at path '#{File.expand_path(value)}'") if !File.exist?(value)
                         end
                     ),
                     FastlaneCore::ConfigItem.new(
+                        key: :podspec_allow_warnings,
+                        env_name: "FL_RELEASE_PODSPEC_ALLOW_WARNINGS",
+                        default_value: true,
+                        description: "Allow warnings during pod push",
+                        optional: true,
+                        is_string: false
+                    ),
+                    FastlaneCore::ConfigItem.new(
                         key: :podspec_repo,
-                        env_name: "PODSPEC_REPO",
+                        env_name: "FL_RELEASE_PODSPEC_REPO",
                         default_value: "Trunk",
                         description: "The repo you want to push. Pushes to Trunk by default",
                         optional: true,
                         type: String
                     ),
                     FastlaneCore::ConfigItem.new(
-                        key: :post_bump,
-                        env_name: "POST_BUMP",
-                        default_value: false,
-                        description: "Bump version number after tag and release",
-                        optional: true,
-                        type: Boolean
-                    ),
-                    FastlaneCore::ConfigItem.new(
-                        key: :pre_bump,
-                        env_name: "PRE_BUMP",
-                        conflicting_options: [:version],
-                        default_value: false,
-                        description: "Bump version number before tag and release",
-                        optional: true,
-                        type: Boolean
-                    ),
-                    FastlaneCore::ConfigItem.new(
-                        key: :sources,
-                        env_name: "SOURCES",
+                        key: :podspec_sources,
+                        env_name: "FL_RELEASE_PODSPEC_SOURCES",
                         default_value: ["https://github.com/CocoaPods/Specs"],
                         description: "The sources of repos you want the pod spec to lint with, separated by commas",
                         optional: true,
@@ -164,7 +161,7 @@ module Fastlane
                     ),
                     FastlaneCore::ConfigItem.new(
                         key: :tag_prefix,
-                        env_name: "TAG_PREFIX",
+                        env_name: "FL_RELEASE_TAG_PREFIX",
                         default_value: "",
                         description: "A prefix to be added to the version tag (e.g \"v/\")",
                         optional: true,
@@ -172,15 +169,23 @@ module Fastlane
                     ),
                     FastlaneCore::ConfigItem.new(
                         key: :version,
-                        env_name: "VERSION",
-                        conflicting_options: [:pre_bump, :version_bump_type],
+                        env_name: "FL_RELEASE_VERSION",
+                        conflicting_options: [:version_bump_type],
                         description: "Change to a specific version. This will replace the bump type value",
                         optional: true,
                         type: String
                     ),
                     FastlaneCore::ConfigItem.new(
+                        key: :version_bump_commit_message,
+                        env_name: "FL_RELEASE_VERSION_BUMP_COMMIT_MESSAGE",
+                        default_value: "[FASTLANE] Version bump",
+                        description: "The commit message when committing the version bump",
+                        optional: true,
+                        type: String
+                    ),
+                    FastlaneCore::ConfigItem.new(
                         key: :version_bump_type,
-                        env_name: "VERSION_BUMP_TYPE",
+                        env_name: "FL_RELEASE_VERSION_BUMP_TYPE",
                         conflicting_options: [:version],
                         default_value: "patch",
                         description: "The type of this version bump. Available: patch, minor, major",
@@ -192,7 +197,9 @@ module Fastlane
                     ),
                     FastlaneCore::ConfigItem.new(
                         key: :xcodeproj,
-                        env_name: "XCODEPROJ",
+                        env_name: "FL_RELEASE_XCODEPROJ",
+                        default_value: Dir["*.xcodeproj"].first,
+                        default_value_dynamic: true,
                         description: "The path of the xcode project to update",
                         optional: true,
                         type: String,
